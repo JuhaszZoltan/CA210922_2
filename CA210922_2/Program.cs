9ee331e4412fdf4a54aa26b0daa402f40e2bb86e
Juhász Zoltán Admin <juhaszz.admin@verebely.dc>
2021-09-22 19:14:35 +0200

    Add project files.

---
+++ CA210922_2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA210922_2
+{
+    public enum UrmertekTipus
+    {
+        Liter,
+        Kanna,
+        Gallon,
+    };
+
+    class Gepjarmu
+    {
+        protected int _benzinMennyisege;
+        public int tankMerete;
+        private int _hengerekSzama;
+        public void Tankol()
+        {
+            _benzinMennyisege = tankMerete;
+        }
+
+        public void Tankol(int mennyiseg, UrmertekTipus u)
+        {
+            switch (u)
+            {
+                case UrmertekTipus.Liter: _benzinMennyisege += mennyiseg;
+                    break;
+                case UrmertekTipus.Kanna:
+                    _benzinMennyisege += mennyiseg * 20;
+                    break;
+                case UrmertekTipus.Gallon:
+                    _benzinMennyisege += (int)(mennyiseg * 4.5);
+                    break;
+            }
+        }
+
+        
+
+
+
+        public Gepjarmu()
+        {
+            _hengerekSzama = 4;
+        }
+
+        public Gepjarmu(int benzinMennyisege) : this()
+        {
+            _benzinMennyisege = benzinMennyisege;
+        }
+
+        public Gepjarmu(int benzinMennyisege, int tankMerete): this(benzinMennyisege)
+        {
+            this.tankMerete = tankMerete;
+        }
+    }
+
+    class Auto : Gepjarmu
+    {
+        public int szallithatoSzemelyekSzama = 4;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            //Auto a = new Auto()
+            //{
+            //    szallithatoSzemelyekSzama = 5,
+            //    tankMerete = 75,
+            //};
+
+            //Gepjarmu babetta = new Gepjarmu()
+            //{
+            //    tankMerete = 10,
+            //};
+
+            //babetta.Tankol(20, UrmertekTipus.Kanna);
+
+
+            Gepjarmu g = new Gepjarmu(100, 1000);
+
+            g.Tankol();
+          
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CA210922_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CA210922_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CA210922_2")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("731c8493-3831-4097-bcb3-010af04291eb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
